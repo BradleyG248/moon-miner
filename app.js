c9398f869f284e6f1a122f53f47768a3d394d957
BradleyG248 <calvinandhobbs28@gmail.com>
2020-01-31 15:27:22 -0700

    Added images for indication

--- app.js
+++ app.js
@@ -1,40 +1,85 @@
-let recruits = 0;
+let recruits = 200000;
 let clickModifier = 1;
 let autoModifier = 0;
+let turn = true;
 let clickUpgrades = [
   {
-    name: 'spy',
-    price: 100,
+    name: "spy",
+    price: 50,
     quantity: 0,
     multiplier: 1
+  },
+  {
+    name: "advisor",
+    price: 1000,
+    quantity: 0,
+    multiplier: 5
+  },
+  {
+    name: "general",
+    price: 5000,
+    quantity: 0,
+    multiplier: 10
   }
 ]
 let automaticUpgrades = [
   {
     name: "village",
-    price: 20,
+    price: 500,
+    quantity: 0,
+    multiplier: 2
+  },
+  {
+    name: "booth",
+    price: 200,
+    quantity: 0,
+    multiplier: 1
+  },
+  {
+    name: "city",
+    price: 2000,
     quantity: 0,
     multiplier: 4
   },
   {
-    name: "hut",
-    price: 50,
+    name: "district",
+    price: 5000,
     quantity: 0,
-    multiplier: 5
+    multiplier: 10
   }
 ]
 function update() {
   document.getElementById("recruit-stat").textContent = ": " + recruits;
 }
+function clickUpdate(type) {
+  document.getElementById(clickUpgrades[type].name).textContent = "" + clickUpgrades[type].price;
+  document.getElementById("click-multipliers").textContent = "Recruits per speech: " + clickModifier;
+}
+function autoUpdate(type) {
+  document.getElementById(automaticUpgrades[type].name + "-cost").textContent = "" + automaticUpgrades[type].price;
+  document.getElementById("auto-multipliers").textContent = "" + autoModifier;
+}
+function turnChange() {
+  turn = true;
+}
 function recruit() {
-  recruits += clickModifier;
-  update();
+  if (turn) {
+    recruits += clickModifier;
+    update();
+    turn = false;
+    setTimeout(turnChange, 100)
+  }
+}
+function setAutoImages(type) {
+  document.getElementById(automaticUpgrades[type].name + "-" + automaticUpgrades[type].quantity).src = automaticUpgrades[type].name + ".png";
 }
 function buyClickUpgrade(type) {
   if (recruits >= clickUpgrades[type].price) {
     clickUpgrades[type].quantity++;
     recruits -= clickUpgrades[type].price;
+    clickUpgrades[type].price = Math.floor(clickUpgrades[type].price *= 2.5);
     setClickUpgrades();
+    clickUpdate(type);
     update();
   }
   else {
@@ -45,8 +90,10 @@ function buyAutoUpgrade(type) {
   if (recruits >= automaticUpgrades[type].price) {
     automaticUpgrades[type].quantity++;
     recruits -= automaticUpgrades[type].price;
-    console.log(automaticUpgrades[type].quantity);
+    automaticUpgrades[type].price = Math.floor(automaticUpgrades[type].price *= 1.1);
     setAutoUpgrades();
+    autoUpdate(type);
+    setAutoImages(type);
   }
   else {
     alert("Not Enough!!!");
